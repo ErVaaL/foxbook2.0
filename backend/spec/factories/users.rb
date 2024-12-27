74135f6887d32317cd83536a190566b48f4fa519
ErVaaL <ErVaLofficial@protonmail.com>
2024-12-27 11:03:11 +0100

    Added unit test for CRUD
    Tests created:
    + adding user to db
    + check if user already exists (conflict)
    + not adding user with wrong parameters
    
    + rspec config for tests

---
+++ backend/spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { 'John Doe' }
+    email { 'john.doe@example.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+end
---
+++ backend/spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe "Users API", type: :request do
+  let(:valid_attributes) do
+    {
+      user: {
+        name: "Jane Doe",
+        email: "jane.doe@example.com",
+        password: "password123",
+        password_confirmation: "password123"
+      }
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      user: {
+        name: "",
+        email: "invalid_email",
+        password: "short",
+        password_confirmation: "mismatch"
+      }
+    }
+  end
+
+  describe "POST /api/v1/users/register" do
+    context "with valid parameters" do
+      it "creates a new user" do
+        expect {
+          post "/api/v1/users/register", params: valid_attributes, as: :json
+        }.to change(User, :count).by(1)
+      end
+
+      it "returns a created response with a token" do
+        valid_attributes[:user][:email] = "new_email@example.com"
+
+        post "/api/v1/users/register", params: valid_attributes, as: :json
+        expect(response).to have_http_status(:created)
+        json = JSON.parse(response.body)
+        expect(json).to have_key("token")
+        expect(json["message"]).to eq("User registered successfully")
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new user" do
+        expect {
+          post "/api/v1/users/register", params: invalid_attributes, as: :json
+        }.to_not change(User, :count)
+      end
+
+      it "returns an unprocessable entity response" do
+        post "/api/v1/users/register", params: invalid_attributes, as: :json
+        expect(response).to have_http_status(:bad_request)
+        json = JSON.parse(response.body)
+        expect(json).to have_key("error")
+        expect(json["error"]).to eq("Failed to create user")
+      end
+    end
+  end
+end
