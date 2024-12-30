6736e498bb5856d82b20a015b0c7f4b0d21451fe
ErVaaL <ErVaLofficial@protonmail.com>
2024-12-30 17:28:23 +0100

    Adjusted the user serializer
    + fields: age, friends, friend_requests_sent, friend_requests_recieved

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :first_name, :last_name, :username, :email, :phone
+  attributes :first_name, :last_name, :username, :age, :email, :phone, :friends, :friend_requests_sent, :friend_requests_recieved
 end
