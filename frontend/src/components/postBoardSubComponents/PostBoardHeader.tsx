db0206709ac8146383eb93b79fa97a412d0c6f90
ErVaaL <ErVaLofficial@protonmail.com>
2025-01-08 01:10:20 +0100

    Prepared for dynamic searching
    Due to backend issues got delayed

--- frontend/src/components/postBoardSubComponents/PostBoardHeader.tsx
+++ frontend/src/components/postBoardSubComponents/PostBoardHeader.tsx
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 
 const PostBoardHeader: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [groups, setGroups] = useState<string[]>([]);
+  const [friends, setFriends] = useState<string[]>([]);
+  const [events, setEvents] = useState<string[]>([]);
+  
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   };
   return (
-    <div className="p-4 flex justify-between items-center">
+    <div className="p-4 flex justify-between items-center gap-3">
+      <h2 className="dark:text-gray-300 p-2">Choose filter:</h2>
+      <select className="dark:bg-gray-600 rounded-lg p-2 dark:text-gray-300">
+        <option value="all">All</option>
+        <option value="friends">Friends</option>
+        <option value="groups">Groups</option>
+        <option value="events">Events</option>
+      </select>
       <input
         type="text"
         value={searchQuery}
