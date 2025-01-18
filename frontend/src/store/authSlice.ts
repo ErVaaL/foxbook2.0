1b2d2f1d238f7932048cf4d1316b74c03cd75eb4
ErVaaL <ErVaLofficial@protonmail.com>
2025-01-18 14:33:04 +0100

    Added user basic data as state object
    + In authSlice now userData is fetched and mapped to state to use
    globally in app

--- frontend/src/store/authSlice.ts
+++ frontend/src/store/authSlice.ts
@@ -1,15 +1,29 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import { API_BASE_URL, API_ENDPOINTS } from "../config";
+
+interface User {
+  id: string;
+  email: string;
+  username: string;
+  avatar?: string;
+}
 
 interface AuthState {
   isLoggedIn: boolean;
   token: string;
   userId: string | null;
+  user: User | null;
 }
+
+const storedUser = localStorage.getItem("user");
+const parsedUser: User | null = storedUser ? JSON.parse(storedUser) : null;
 
 const initialState: AuthState = {
   isLoggedIn: false,
   token: "",
-  userId: null,
+  userId: parsedUser?.id || null,
+  user: parsedUser,
 };
 
 const getUserIdFromToken = (token: string): string | null => {
@@ -24,6 +38,39 @@ const getUserIdFromToken = (token: string): string | null => {
   }
 };
 
+export const fetchUserData = createAsyncThunk(
+  "auth/fetchUserData",
+  async (token: string, { rejectWithValue }) => {
+    const userId = getUserIdFromToken(token);
+    try {
+      if (!userId) throw new Error("Invalid user ID");
+      const response = await axios.get(
+        `${API_BASE_URL}${API_ENDPOINTS.USER(userId)}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const userData = response.data.user.data.attributes;
+      if (!userData) throw new Error("Invalid user data");
+      return {
+        id: response.data.user.data.id || "",
+        email: userData.email || "",
+        username: userData.username || "",
+        avatar: userData.avatar || "",
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(
+          error.response?.data || `Failed to fetch user data`,
+        );
+      }
+    }
+  },
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -34,12 +81,15 @@ const authSlice = createSlice({
       state.isLoggedIn = true;
       state.token = token;
       state.userId = getUserIdFromToken(token);
+      state.user = parsedUser;
     },
     logout: (state) => {
       sessionStorage.removeItem("authToken");
+      sessionStorage.removeItem("user");
       state.isLoggedIn = false;
       state.token = "";
       state.userId = null;
+      state.user = null;
     },
     validateToken: (state) => {
       const token = sessionStorage.getItem("authToken");
@@ -69,6 +119,16 @@ const authSlice = createSlice({
         state.userId = null;
       }
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserData.fulfilled, (state, action) => {
+        state.user = action.payload;
+        localStorage.setItem("user", JSON.stringify(action.payload));
+      })
+      .addCase(fetchUserData.rejected, (state) => {
+        state.user = null;
+      });
   },
 });
 
