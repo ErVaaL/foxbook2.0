d81db90ff75766ca6e15a0ed7f1f9cbd9d9fbef1
ErVaaL <ErVaLofficial@protonmail.com>
2025-01-18 18:58:54 +0100

    Added few of settings to user serializer
    + Now for frontend user object it will have info about privacy and
    notifications for user

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,4 +1,11 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :id, :first_name, :last_name, :username, :birthday, :email, :phone, :avatar, :friends, :friend_requests_sent, :friend_requests_recieved
+
+  attribute :settings do |user|
+    {
+      privacy: user.settings&.privacy || "private",
+      notifications: user.settings&.notifications || true
+    }
+  end
 end
